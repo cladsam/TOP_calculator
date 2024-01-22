dcd1dedcafe838828d267692b9451b61dc08efe8
Morgan Bourgeois <19302449+cladsam@users.noreply.github.com>
2024-01-22 10:20:00 +0100

    add some event handlers and display pressed number

--- javascript.js
+++ javascript.js
@@ -8,6 +8,35 @@ const operators = {
 const formatERROR = "formatERROR"
 
 
+let numberA;
+let numberB;
+let operator;
+let displayText = "";
+
+const btnNumbers = document.querySelectorAll(".numberBtn");
+const displayZone = document.querySelector("#display");
+const btnMultiply = document.querySelector("#multiply");
+const btnDivide = document.querySelector("#divide");
+const btnSoustract = document.querySelector("#divide");
+const btnAdd = document.querySelector("#add");
+const btnSqrt = document.querySelector("#sqrt")
+const btnDot = document.querySelector("#dot")
+const btnBack = document.querySelector("#back")
+const btnClear = document.querySelector("#clear");
+
+for (button of btnNumbers) {
+
+    button.addEventListener("click", (e) => { pressNumber(e) })
+}
+
+function pressNumber(e) {
+    displayText += e.target.textContent;
+    console.log(displayText);
+    updateDisplay(displayText)
+}
+function updateDisplay(displayText) {
+    displayZone.textContent = displayText;
+}
 function add(a, b) {
     return a + b;
 }
